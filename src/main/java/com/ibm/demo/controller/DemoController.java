969721cbf1010927b1661b18aa10b422059ece39
JackSilence <jacksilence@gmail.com>
2018-02-10 13:30:00 +0800

    修改ServiceInstance的取得方式

--- src/main/java/com/ibm/demo/controller/DemoController.java
+++ src/main/java/com/ibm/demo/controller/DemoController.java
@@ -1,8 +1,11 @@
 package com.ibm.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +13,8 @@ import com.google.common.collect.ImmutableMap;
 
 @RestController
 public class DemoController {
-	// @Autowired
-	// private Registration registration;
+	@Autowired
+	private Registration registration;
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
@@ -21,9 +24,10 @@ public class DemoController {
 		return ImmutableMap.of( "key", "value" );
 	}
 
-	@SuppressWarnings( "deprecation" )
 	@GetMapping( "/instance-info" )
 	public ServiceInstance showInfo() {
-		return this.discoveryClient.getLocalServiceInstance();
+		List<ServiceInstance> list = discoveryClient.getInstances( registration.getServiceId() );
+
+		return list.isEmpty() ? null : list.get( 0 );
 	}
 }
